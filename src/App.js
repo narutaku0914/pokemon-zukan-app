65113cfe97903000e6cd4c815fe5a839ba6cbf2f
narutaku0914 <tkm.nrsw@gmail.com>
2022-08-18 19:34:14 +0900

    pokemonデータ取得

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+  useEffect(() => {
+    const fetchPokemondata = async () => {
+      let res = await getAllPokemon(initialUrl);
+      console.log(res);
+    };
+    fetchPokemondata();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
